20f0c1c97f475159239933a0349b2a3a4a24bf1c
MrAli-a11 <mrali9132@gmail.com>
2026-04-10 21:19:50 +0530

    Learned about Data Definition Language & Data Manupulation Language

---
+++ DML.sql
@@ -0,0 +1,10 @@
+-- [INSERT INTO]
+
+
+INSERT INTO customers (id, first_name, country, score)	
+VALUES 
+		(6, 'NAMER', 'POURTGAL', 880),
+		(7, 'SAMIR', NULL, 6700),
+		(8, 'ISMA', 'TURKIYE', NULL)
+
+SELECT * FROM customers
